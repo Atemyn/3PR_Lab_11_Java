4b70fa73cc2ba6a0237e7e490f87a81f12496a71
Atemyn <atemyn@mail.ru>
2021-01-01 21:10:33 +0300

    Исправлены ошибки

--- PR_Lab_11_Java.java
+++ PR_Lab_11_Java.java
@@ -8,14 +8,9 @@ public class PR_Lab_11_Java{
 	public static void main(String[] args){
 		/* Инициализация массива. */
 		Building mB[] = new Building[5];
-		int i;
-		for(i = 0; i < 5; i++)
+		for(int i = 0; i < 5; i++)
 		{
 			mB[i] = new Building(i);
 		}
-		
-		b1.getBuilding();
-		b2.getBuilding();
-		b3.getBuilding();
 	}
 }
