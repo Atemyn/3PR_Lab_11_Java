2f84b5a43386ed6448cc650fa82b0c995c2dd694
Atemyn <atemyn@mail.ru>
2021-01-01 21:08:30 +0300

    Начало ЛР 11

---
+++ PR_Lab_11_Java.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+import myClasses.Facade;
+import myClasses.Building;
+import myClasses.HelpClass;
+
+public class PR_Lab_11_Java{
+	public static void main(String[] args){
+		/* Инициализация массива. */
+		Building mB[] = new Building[5];
+		int i;
+		for(i = 0; i < 5; i++)
+		{
+			mB[i] = new Building(i);
+		}
+		
+		b1.getBuilding();
+		b2.getBuilding();
+		b3.getBuilding();
+	}
+}
